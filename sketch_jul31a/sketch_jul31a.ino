7127d5b694a4b67e127d32b446f4137565b5c8ac
Francesco Arzon <franz9700@gmail.com>
2023-09-15 14:33:20 +0200

    Funziona lettura tempo tag
    Usata funzione PICC_WakeupA() per resettare campo del picc e poter leggere più volte il tag

--- sketch_jul31a/sketch_jul31a.ino
+++ sketch_jul31a/sketch_jul31a.ino
@@ -1,72 +1,61 @@
-/*
- * This ESP32 code is created by esp32io.com
- * This ESP32 code is released in the public domain
- * For more detail (instruction and wiring diagram), visit https://esp32io.com/tutorials/esp32-rfid-nfc
- */
-#include <PubSubClient.h>
 #include <SPI.h>
 #include <MFRC522.h>
-#include <WiFi.h>
 
 #define SS_PIN 5    // ESP32 pin GPIO5
 #define RST_PIN 27  // ESP32 pin GPIO27
 
-const char* ssid = "TP-Link";
-const char* password = "WbdL57ak12";
-const char* broker = "192.168.1.110";
-const char* topic = "test";
-const int port = 1883;
-
 MFRC522 rfid(SS_PIN, RST_PIN);
-WiFiClient WIFIclient;
-PubSubClient client(WIFIclient);
+
+unsigned long timeStart = 0;
+unsigned long timeEnd = 0;
+bool tagPresent = false;
+
+byte bufferATQA[2];
+byte bufferSize = sizeof(bufferATQA);
 
 void setup() {
   Serial.begin(9600);
-
-  WiFi.begin(ssid, password);
-  Serial.print("Connecting to Wifi...");
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(2000);
-    Serial.print(".");
-  }
-  Serial.println("");
-
-  client.setServer(broker, port);
-
-  while (!client.connected()) {
-    Serial.println("Connecting to MQTT Broker ...");
-
-    if (client.connect("ESP32-cardReader")) {
-      Serial.println("Connected to Broker as ESP32-cardReader");
-    } else {
-      Serial.print("Failed with state ");
-      Serial.println(client.state());
-      delay(1000);
-    }
-  }
-
   SPI.begin();      // init SPI bus
   rfid.PCD_Init();  // init MFRC522
-
-  Serial.println("Tap an RFID/NFC tag on the RFID-RC522 reader");
+  Serial.println("Use an RFID tag");
 }
 
-void loop() {
-  if (rfid.PICC_IsNewCardPresent()) {  // new tag is available
-    Serial.println("NEW CARD PRESENT");
-    //client.publish("test", "RFID letto\n");
-    if (rfid.PICC_ReadCardSerial()) { 
-      long int t1= millis();
-      while(rfid.){
-        Serial.print('Spillando\n');
-        delay(1000);
-      }
-      long int t2= millis();
-      Serial.println(t2-t1);
+//Non so perché devo mettere 3 volte il wakeup ma così funziona, provato a caso
+//Se elimino anche uno non funziona quindi non toccare
 
-     // Serial.println("READ CARD SERIAL");
+void loop() {
+  MFRC522::StatusCode status = rfid.PICC_WakeupA(bufferATQA, &bufferSize);
+  //Serial.print("Prima del controllo - ");
+  //Serial.println(status, DEC);
+  delay(500);
+
+  if (rfid.PICC_IsNewCardPresent() && rfid.PICC_ReadCardSerial()) {
+    status = rfid.PICC_WakeupA(bufferATQA, &bufferSize);
+    //Serial.print("True - ");
+    //Serial.println(status, DEC);
+    //delay(500);
+    
+    if (!tagPresent) {  // if tagPresent = false, then the tag has just "arrived"
+      tagPresent = true;
+      timeStart = millis();
+      Serial.println("Tag NFC RILEVATO!");
+    }
+
+  } else {
+    if (tagPresent) {  //at some point there was a tag, but it's gone now
+      Serial.print("\n");
+      timeEnd = millis();
+      unsigned long duration = timeEnd - timeStart;
+      Serial.print("Tag NFC stayed for ");
+      Serial.print(duration);
+      Serial.println(" ms");
+     //  noted the last tag's time, reset
+      tagPresent = false;
     }
   }
-  //sleep(1);
+  status = rfid.PICC_WakeupA(bufferATQA, &bufferSize);
+  //Serial.print("Fuori da controllo - ");
+  //Serial.println(status, DEC);
+  //delay(500);
+
 }
